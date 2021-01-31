0cd68653209feba86740fe6de244af4ce449b5bf
Yusuke Kido <mail@yusuk.eki.do>
2021-01-31 11:12:40 +0900

    OGP

--- config.ts
+++ config.ts
@@ -1,6 +1,7 @@
 export const config: Config = {
   dataRootDir: '../.src',
   dataGitHubRepo: 'kissge/yo.eki.do',
+  host: 'https://yo.eki.do',
   title: '葉月夜堂',
   googleAnalyticsTrackingID: 'UA-45102742-1',
 };
