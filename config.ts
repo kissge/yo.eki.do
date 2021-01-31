53a43822bdec240378940637cfc4f40839e113a8
Yusuke Kido <mail@yusuk.eki.do>
2021-01-31 11:45:35 +0900

    Webmention

--- config.ts
+++ config.ts
@@ -3,5 +3,6 @@ export const config: Config = {
   dataGitHubRepo: 'kissge/yo.eki.do',
   host: 'https://yo.eki.do',
   title: '葉月夜堂',
+  twitter: 'p_km',
   googleAnalyticsTrackingID: 'UA-45102742-1',
 };
