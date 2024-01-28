1a5737325ef3baa1534953c03fe008e87d5c77c0
kissge <kissge@users.noreply.github.com>
2024-01-28 07:47:28 +0000

    deploy: d818c41feb1a2434bcace714fb24177acc9b6029

---
+++ _app/immutable/chunks/paths.4Ub15Yvt.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_cm87r3?.base??"",a=globalThis.__sveltekit_cm87r3?.assets??s;export{a,s as b};
--- _app/immutable/chunks/paths.Z5E7SFjX.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_x7wohc?.base??"",a=globalThis.__sveltekit_x7wohc?.assets??s;export{a,s as b};
